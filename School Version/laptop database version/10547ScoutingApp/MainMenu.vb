8b487958efdd0e3023c5bf9b95078175045993ab
Dusty <dbeehler00@gmail.com>
2017-12-01 14:46:05 -0600

    added version 2 with database for laptop

---
+++ School Version/laptop database version/10547ScoutingApp/MainMenu.vb
@@ -0,0 +1,32 @@
+﻿Public Class MainMenu
+    Private Sub MainMenu_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.CenterToScreen()
+        rankingsForm.Visible = False
+    End Sub
+
+    Private Sub btnExcel_Click(sender As Object, e As EventArgs) Handles btnExcel.Click
+        Me.Visible = False
+        ExcelExporter.Visible = True
+    End Sub
+
+    Public Shared Sub applicationClose()
+        TeamImport.Close()
+        TextExporter.Close()
+        MainMenu.Close()
+        ExcelExporter.Close()
+        rankingsForm.Close()
+    End Sub
+
+    Private Sub BtnText_Click(sender As Object, e As EventArgs) Handles BtnText.Click
+        Dim frmTextExporter As New TextExporter
+        frmTextExporter.ShowDialog()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        applicationClose()
+    End Sub
+
+    Private Sub btnViewRankings_Click(sender As Object, e As EventArgs) Handles btnViewRankings.Click
+        rankingsForm.Visible = True
+    End Sub
+End Class
---
+++ School Version/laptop database version/10547ScoutingApp/rankingsForm.vb
@@ -0,0 +1,44 @@
+﻿Imports System.Data.SqlClient
+Imports System.Data.OleDb
+Public Class rankingsForm
+    Private Sub rankingsForm_Load_1(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'FinalMainDataDataSet.Table' table. You can move, or remove it, as needed.
+        Me.TableTableAdapter.Fill(Me.FinalMainDataDataSet.Table)
+        'starts the editing
+        Me.TableBindingSource.AddNew()
+    End Sub
+
+    Private Sub SaveAndCloseToolStripMenuItem_Click(sender As Object, e As EventArgs)
+        Try
+            'saves entries
+            Me.TableBindingSource.EndEdit()
+            'updates table
+            Me.TableTableAdapter.Update(Me.FinalMainDataDataSet.Table)
+            'closes app
+            Me.Visible = False
+        Catch ex As Exception
+            'catches if an error
+            MessageBox.Show(Me, "Error: " & ex.Message, "Save", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End Try
+    End Sub
+
+    'doesnt work D:
+    Private Sub ClearDataToolStripMenuItem_Click(sender As Object, e As EventArgs)
+        Dim result As Integer = MessageBox.Show("Are you sure?", "Warning", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning)
+        If result = DialogResult.Cancel Then
+            MessageBox.Show("Aborted")
+        ElseIf result = DialogResult.No Then
+            MessageBox.Show("Aborted")
+        ElseIf result = DialogResult.Yes Then
+        End If
+    End Sub
+
+    Private Sub ExitWithoutSaveToolStripMenuItem_Click(sender As Object, e As EventArgs)
+        'cancels edit
+        Me.TableBindingSource.CancelEdit()
+    End Sub
+
+    Private Sub SaveAndCloseToolStripMenuItem_Click_1(sender As Object, e As EventArgs) Handles SaveAndCloseToolStripMenuItem.Click
+
+    End Sub
+End Class
